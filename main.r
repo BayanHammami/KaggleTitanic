590909c5899e3853e816159fb7b1e7f1792ac679
BayanHammami <bayan.hammami@gmail.com>
2018-04-01 11:47:55 +1000

    Added example submission

--- main.r
+++ main.r
@@ -1,10 +1,33 @@
 library(tidyverse)
+library(caret)
 
 genderSubmission <- read_csv('./data/gender_submission.csv')
 test <- read_csv('./data/test.csv')
 train <- read_csv('./data/train.csv')
 
-View(train)
-View(test)
+train$Survived <- paste0('val_', train$Survived)
+genderSubmission$Survived <- paste0('val_', genderSubmission$Survived)
+train$Pclass <- paste0('val_', train$Pclass)
+test$Pclass <- paste0('val_', test$Pclass)
 
-str(train)
+fitControl <- trainControl(
+  method = "repeatedcv",
+  number = 10,
+  repeats = 1)
+
+fit <- train(Survived ~ Pclass + Sex, data = train, 
+                 method = "xgbTree", 
+                 trControl = fitControl,
+                 verbose = TRUE)
+
+varImp(fit)
+
+predictions <- predict(fit, newdata = test)
+
+confusionMatrix(predictions, as.factor(genderSubmission$Survived))
+
+
+
+
+
+
